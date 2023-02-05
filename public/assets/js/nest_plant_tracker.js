994a7f2410eb25e982383f40a2190ff96c05aeab
sarahemm <github@sen.cx>
2023-02-05 23:41:06 +0000

    Can now filter to show only plants that need watering, in various ways.

---
+++ public/assets/js/nest_plant_tracker.js
@@ -0,0 +1,30 @@
+function updateWateringDaysText() {
+  var new_val = $(within_watering_days).val();
+
+  if(new_val == 0) {
+    $(watering_days_label).text("Needs water today (or past-due)");
+  } else if(new_val == 1) {
+    $(watering_days_label).text("Needs water today or tomorrow (or past-due)");
+  } else {
+    $(watering_days_label).text("Within " + new_val + " days of needing water (or past-due)");
+  }
+}
+
+function updateNotCheckedInText() {
+  var new_val = $(not_checked_in).val();
+
+  if(new_val == 0) {
+    $(not_checked_in_label).text("");
+  } else if(new_val == 1) {
+    $(not_checked_in_label).text("and hasn't been checked today");
+  } else if(new_val == 2) {
+    $(not_checked_in_label).text("and hasn't been checked yesterday or today");
+  } else {
+    $(not_checked_in_label).text("and hasn't been checked in the last " + new_val + " days");
+  }
+}
+
+$( document ).ready(function() {
+  updateWateringDaysText();
+  updateNotCheckedInText();
+});
